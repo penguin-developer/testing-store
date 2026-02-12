e8d8412d60c7f6ab1743ec6e05d350985c227acb
penguin-developer <thor462442@gmail.com>
2026-02-11 20:23:51 -0600

    Update myfile.lua

--- myfile.lua
+++ myfile.lua
@@ -579,7 +579,7 @@ local function onNext(isbuyer)
     end
 
     local lastAttackTime = 0
-    local ATTACK_COOLDOWN = 0.12
+    local ATTACK_COOLDOWN = 0.08
 
     local function attacksMelee(humanoid, myStats, pos)
         if isFreezeAttacksMelee or isModeAutoTransform then
